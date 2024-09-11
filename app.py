a319ee0d7de5fdd42e4acd25d1abe8ea6577cca5
Giacomo Fagioli <gf@gfcyb.org>
2024-09-11 08:18:17 +0200

    just adding some headers and output

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
-import os
-import time
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Filename: app.py
+Author: Giacomo Fagioli
+Email: gf [AT] gfcyb [DOT] org
+Date: 2024-09-11
+Version: 0.1
+License: GPL3
+Repo: https://github.com/GFcyborg/test0
+Credits: Alan Turing, Claude Shannon, Linus Torvalds, Guido van Rossum
+Description: This header is called obj.__doc__
+"""
+
+__author__ = "Giacomo Fagioli"
+__status__ = "Production"
+
+import os
+import datetime
+
+now = datetime.datetime.now()
+print(f"Now it's : { now.strftime("%Y-%m-%d %H:%M:%S") }.")
